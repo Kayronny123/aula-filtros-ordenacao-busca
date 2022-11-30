9f9468069cd0547761f4ffc5b3cf449d6905bb99
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2022-11-30 20:55:26 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import styled, { createGlobalStyle } from "styled-components";
+import pokemons from "./pokemon/pokemon.json";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+import { getColors } from "./utils/ReturnCardColor";
+import Header from "./components/Header/Header.js";
+import { useState } from "react";
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+  
+  }
+`;
+const CardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
+  justify-items: center;
+`;
+
+function App() {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  return (
+    <>
+      <GlobalStyle />
+      <Header id={id} setId={setId} name={name} setNome={setName} />
+      <CardsContainer>
+        {pokemons
+          .filter((pokemon) => {
+            return pokemon.id.includes(id);
+          })
+          .filter((pokemon) => {
+            return pokemon.name.english.includes(name);
+          })
+          .map((pokemon) => {
+            return (
+              <PokemonCard
+                cardColor={getColors(pokemon.type[0])}
+                key={pokemon.id}
+                pokemon={pokemon}
+              />
+            );
+          })}
+      </CardsContainer>
+    </>
+  );
+}
+
+export default App;
